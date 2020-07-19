6e423e66f54d596e3498f8a58432ebaf95ab3234
Artem Kundakchan <kundakchan@yandex.ru>
2020-07-19 18:57:30 +0300

    Общие настройки webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,108 @@
 const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin')
+const TerserWebpackPlugin = require('terser-webpack-plugin')
+
+const isDev = process.env.NODE_ENV === 'development'
+const isProd = process.env.NODE_ENV === 'production'
+
+const optimization = () => {
+  const config = {
+    splitChunks: {
+      chunks: 'all'
+    }
+  }
+  if (isProd) {
+    config.minimizer = [
+      new OptimizeCssAssetsWebpackPlugin(),
+      new TerserWebpackPlugin()
+    ]
+  }
+  return config
+}
+const filename = ext => isDev ? `[name].${ext}` : `[name].[hash].${ext}`
+const cssLoader = extra => {
+  const loaders = [
+    {
+      loader: MiniCssExtractPlugin.loader,
+      options: {
+        hmr: isDev,
+        reloadAll: true
+      }
+    },
+    'css-loader'
+  ]
+  if (extra) loaders.push(extra)
+  return loaders
+}
 
 module.exports = {
+  context: path.resolve(__dirname, 'src'),
   mode: 'development',
-  entry: './src/index.js',
+  entry: {
+  main: ['@babel/polyfill', './index.js']
+  },
   output: {
-    filename: 'bundle.js',
+    filename: filename('js'),
     path: path.resolve(__dirname, 'dist')
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@assets': path.resolve(__dirname, 'src/assets'),
+      '@style': path.resolve(__dirname, 'src/scss')
+    }
+  },
+  optimization: optimization(),
+  devtool: isDev ? 'source-map' : '',
+  devServer: {
+    port: 3000,
+    hot: isDev
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: './index.html',
+      minify: {
+        collapseWhitespace: isProd
+      }
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: filename('css')
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: cssLoader()
+      },
+      {
+        test: /\.(scss|sass)$/,
+        use: cssLoader('sass-loader')
+      },
+      {
+        test: /\.(png|jpg|svg|gif)$/,
+        use: ['file-loader']
+      },
+      {
+        test: /\.(ttf|woff|woff2|eot)$/,
+        use: ['file-loader']
+      },
+      {
+        test: /\.js$/,
+        exclude: '/node_modules',
+        loader: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env'
+            ]
+          }
+        }
+      }
+    ]
   }
 }
